5ea90d8d28bcd4bf9feae7401bf741a4e0abf89d
legsim <simon@legtux.org>
2016-06-15 14:26:23 +0200

    Receiver cleanup

--- Linux/receiver.c
+++ Linux/receiver.c
@@ -15,11 +15,6 @@
 #include "consts.h"
 #include "video.h"
 
-/*
-	 const int SCREEN_WIDTH = WIDTH_2;
-	 const int SCREEN_HEIGHT = HEIGHT_2;
-	 */
-
 const int SCREEN_WIDTH = WIDTH;
 const int SCREEN_HEIGHT = HEIGHT;
 
@@ -35,30 +30,15 @@ typedef struct yuv_pic
 
 
 
-
-
 int pipeToF[2];
 int pipeFromF[2];
-/*
-	 int imgSize = WIDTH_2*HEIGHT_2*3/2;
-	 int offsetY = WIDTH_2*HEIGHT_2;
-	 unsigned char buffer[WIDTH_2*HEIGHT_2*3/2];
-	 unsigned char buffer2[WIDTH*HEIGHT*3/2];
-	 */
 
 int imgSize = WIDTH_2*HEIGHT_2*3/2;
 int offsetY = WIDTH*HEIGHT;
 unsigned char buffer[WIDTH_2*HEIGHT_2*3/2];
 unsigned char buffer2[WIDTH*HEIGHT*3/2];
 
-int n_read = 0 ;
-int n = 0;
-
-unsigned char globPkt[300];
-
 pthread_mutex_t mutex;
-pthread_mutex_t mutexCb;
-pthread_cond_t cond;
 
 void addFutureZoi(shared_t *shared, zoiInfo *zoi) {
 	pthread_mutex_lock(&mutex);
@@ -80,7 +60,7 @@ void updateZoi(shared_t *shared, int frame) {
 		if (shared->zoiList->frame <= frame) {
 			shared->zoiX = shared->zoiList->x;
 			shared->zoiY = shared->zoiList->y;
-			
+
 			zoiInfo *toDelete = shared->zoiList;
 			shared->zoiList = shared->zoiList->next;
 			free(toDelete);
@@ -94,238 +74,126 @@ void updateZoi(shared_t *shared, int frame) {
 }
 
 void *task_video(void *data) {
+
 	shared_t *shared = (shared_t*)data;
-	/*
-		 SDL_Surface * ps_screen;
-		 SDL_Overlay * ps_bmp;
-		 SDL_Rect s_rect;
 
-		 char * pv_frame = NULL;
-		 int v_pic_size = 0, v_frame_size = 0;
-		 S_picture s_yuv_pic;
+	SDL_Surface * ps_screen;
+	SDL_Overlay * ps_bmp;
+	SDL_Rect s_rect;
 
-		 if (SDL_Init (SDL_INIT_EVERYTHING) == -1)
-		 {
-		 fprintf (stderr, "Could not initialize SDL -% s \n", SDL_GetError ());
-		 return 1;
-		 }
+	char * pv_frame = NULL;
+	int v_pic_size = 0, v_frame_size = 0;
+	S_picture s_yuv_pic;
 
-		 ps_screen = SDL_SetVideoMode (SCREEN_WIDTH, SCREEN_HEIGHT, 0, SDL_SWSURFACE);
-		 if (! ps_screen)
-		 {
-		 fprintf (stderr, "SDL: could not set video mode-exiting \n");
-		 return 1;
-		 }
-
-		 SDL_WM_SetCaption ("YUV Window", NULL);
-
-		 ps_bmp = SDL_CreateYUVOverlay (SCREEN_WIDTH, SCREEN_HEIGHT, SDL_IYUV_OVERLAY,
-		 ps_screen);
-
-		 v_frame_size = (SCREEN_WIDTH * SCREEN_HEIGHT);
-		 v_pic_size = ((v_frame_size * 3) / 2);
-		 if ((pv_frame = (char *) calloc (v_pic_size, 1)) == NULL)
-		 {
-		 fprintf (stderr, "SYS: could not allocate mem space \n");
-		 return 1;
-		 }
-
-		 s_yuv_pic.pv_data [0] = buffer;
-		 s_yuv_pic.pv_data [2] = (char *) (buffer + v_frame_size);
-		 s_yuv_pic.pv_data [1] = (char *) (buffer + v_frame_size + v_frame_size / 4);
-		 s_yuv_pic.v_linesize [0] = SCREEN_WIDTH;
-		 s_yuv_pic.v_linesize [1] = (SCREEN_WIDTH / 2);
-		 s_yuv_pic.v_linesize [2] = (SCREEN_WIDTH / 2);
-
-		 ps_bmp-> pixels [0] = (unsigned char *) (s_yuv_pic.pv_data [0]);
-		 ps_bmp-> pixels [2] = (unsigned char *) (s_yuv_pic.pv_data [1]);
-		 ps_bmp-> pixels [1] = (unsigned char *) (s_yuv_pic.pv_data [2]);
-
-		 ps_bmp-> pitches [0] = s_yuv_pic.v_linesize [0];
-		 ps_bmp-> pitches [2] = s_yuv_pic.v_linesize [1];
-		 ps_bmp-> pitches [1] = s_yuv_pic.v_linesize [2];
-
-		 s_rect.x = 0;
-		 s_rect.y = 0;
-		 s_rect.w = SCREEN_WIDTH +8;
-		 s_rect.h = SCREEN_HEIGHT +8;
-
-		 int i = 0, ret = 0;
-
-		 while (1) {
-
-		 int zoiX = shared->zoiX;
-		 int zoiY = shared->zoiY;
-
-		 n_read = 0;
-		 while (n_read < imgSize) {
-		 while (( n = read(0, &buffer[n_read], imgSize - n_read)) > 0) {
-		 n_read += n;
-		 }
-		 }
-
-		 i++;
-
-	SDL_LockYUVOverlay (ps_bmp);
-
-	SDL_DisplayYUVOverlay (ps_bmp, & s_rect);
-
-	SDL_UnlockYUVOverlay (ps_bmp);
-
-	SDL_Delay (40);
-}
-*/
-
-SDL_Surface * ps_screen;
-SDL_Overlay * ps_bmp;
-SDL_Rect s_rect;
-
-char * pv_frame = NULL;
-int v_pic_size = 0, v_frame_size = 0;
-S_picture s_yuv_pic;
-
-if (SDL_Init (SDL_INIT_EVERYTHING) == -1)
-{
-	fprintf (stderr, "Could not initialize SDL -% s \n", SDL_GetError ());
-	return NULL;
-}
-
-ps_screen = SDL_SetVideoMode (SCREEN_WIDTH, SCREEN_HEIGHT, 0, SDL_SWSURFACE);
-if (! ps_screen)
-{
-	fprintf (stderr, "SDL: could not set video mode-exiting \n");
-	return NULL;
-}
-
-shared->sdlReady = 1;
-
-SDL_WM_SetCaption ("SnR Player", NULL);
-
-ps_bmp = SDL_CreateYUVOverlay (SCREEN_WIDTH, SCREEN_HEIGHT, SDL_IYUV_OVERLAY,
-		ps_screen);
-
-v_frame_size = (SCREEN_WIDTH * SCREEN_HEIGHT);
-v_pic_size = ((v_frame_size * 3) / 2);
-if ((pv_frame = (char *) calloc (v_pic_size, 1)) == NULL)
-{
-	fprintf (stderr, "SYS: could not allocate mem space \n");
-	return NULL;
-}
-
-s_yuv_pic.pv_data [0] = buffer2;
-s_yuv_pic.pv_data [2] = (char *) (buffer2 + v_frame_size);
-s_yuv_pic.pv_data [1] = (char *) (buffer2 + v_frame_size + v_frame_size / 4);
-s_yuv_pic.v_linesize [0] = SCREEN_WIDTH;
-s_yuv_pic.v_linesize [1] = (SCREEN_WIDTH / 2);
-s_yuv_pic.v_linesize [2] = (SCREEN_WIDTH / 2);
-
-ps_bmp-> pixels [0] = (unsigned char *) (s_yuv_pic.pv_data [0]);
-ps_bmp-> pixels [2] = (unsigned char *) (s_yuv_pic.pv_data [1]);
-ps_bmp-> pixels [1] = (unsigned char *) (s_yuv_pic.pv_data [2]);
-
-ps_bmp-> pitches [0] = s_yuv_pic.v_linesize [0];
-ps_bmp-> pitches [2] = s_yuv_pic.v_linesize [1];
-ps_bmp-> pitches [1] = s_yuv_pic.v_linesize [2];
-
-s_rect.x = 0;
-s_rect.y = 0;
-s_rect.w = SCREEN_WIDTH +8;
-s_rect.h = SCREEN_HEIGHT +8;
-
-int i = 0, ret = 0;
-
-while (1) {
-
-	int zoiX = shared->zoiX;
-	int zoiY = shared->zoiY;
-
-	n_read = 0;
-	while (n_read < imgSize) {
-		while (( n = read(0, &buffer[n_read], imgSize - n_read)) > 0) {
-			n_read += n;
-		}
+	if (SDL_Init (SDL_INIT_EVERYTHING) == -1)
+	{
+		fprintf (stderr, "Could not initialize SDL -% s \n", SDL_GetError ());
+		return NULL;
 	}
 
-	i++;
+	ps_screen = SDL_SetVideoMode (SCREEN_WIDTH, SCREEN_HEIGHT, 0, SDL_SWSURFACE);
+	if (! ps_screen)
+	{
+		fprintf (stderr, "SDL: could not set video mode-exiting \n");
+		return NULL;
+	}
 
-	bloc_t *bloc = malloc(sizeof(bloc_t));
-	bloc_t *blocs = malloc(WIDTH*HEIGHT*sizeof(bloc_t));
-	bloc_t *blocsUp = NULL;
+	shared->sdlReady = 1;
 
-	// ZOI	
-	readBlocs(buffer, WIDTH/2, blocs, 0, HEIGHT-ZOIH, ZOIW, ZOIH);
-	writeBlocs(buffer2, WIDTH, blocs, zoiX, zoiY, ZOIW, ZOIH);
+	SDL_WM_SetCaption ("SnR Player", NULL);
 
-	// LEFT SIDE
-	readBlocs(buffer, WIDTH/2, blocs, 0, 0, zoiX/2, ZOIH);
-	blocsUp = upSampleBlocs(blocs, zoiX/2, ZOIH, 2);		
-	writeBlocs(buffer2, WIDTH, blocsUp, 0, 0, zoiX, HEIGHT);
-	free(blocsUp);
+	ps_bmp = SDL_CreateYUVOverlay (SCREEN_WIDTH, SCREEN_HEIGHT, SDL_IYUV_OVERLAY,
+			ps_screen);
 
-	// RIGHT SIDE
-	readBlocs(buffer, WIDTH/2, blocs, zoiX/2, 0, (ZOIW-zoiX)/2, ZOIH);
-	blocsUp = upSampleBlocs(blocs, (ZOIW-zoiX)/2, ZOIH, 2);
-	writeBlocs(buffer2, WIDTH, blocsUp, zoiX+ZOIW, 0, ZOIW-zoiX, HEIGHT);
-	free(blocsUp);
+	v_frame_size = (SCREEN_WIDTH * SCREEN_HEIGHT);
+	v_pic_size = ((v_frame_size * 3) / 2);
+	if ((pv_frame = (char *) calloc (v_pic_size, 1)) == NULL)
+	{
+		fprintf (stderr, "SYS: could not allocate mem space \n");
+		return NULL;
+	}
 
-	// TOP BLOCK
-	readBlocs(buffer, WIDTH/2, blocs, ZOIW/2, 0, ZOIW/2, zoiY/2);
-	blocsUp = upSampleBlocs(blocs, ZOIW/2, zoiY/2, 2);
-	writeBlocs(buffer2, WIDTH, blocsUp, zoiX, 0, ZOIW, zoiY);
-	free(blocsUp);
+	s_yuv_pic.pv_data [0] = buffer2;
+	s_yuv_pic.pv_data [2] = (char *) (buffer2 + v_frame_size);
+	s_yuv_pic.pv_data [1] = (char *) (buffer2 + v_frame_size + v_frame_size / 4);
+	s_yuv_pic.v_linesize [0] = SCREEN_WIDTH;
+	s_yuv_pic.v_linesize [1] = (SCREEN_WIDTH / 2);
+	s_yuv_pic.v_linesize [2] = (SCREEN_WIDTH / 2);
 
-	// BOTTOM BLOCK
-	readBlocs(buffer, WIDTH/2, blocs, ZOIW/2, zoiY/2, ZOIW/2, (ZOIH-zoiY)/2);
-	blocsUp = upSampleBlocs(blocs, ZOIW/2, (ZOIH-zoiY)/2, 2);
-	writeBlocs(buffer2, WIDTH, blocsUp, zoiX, zoiY+ZOIH, ZOIW, ZOIH-zoiY);
-	free(blocsUp);
+	ps_bmp-> pixels [0] = (unsigned char *) (s_yuv_pic.pv_data [0]);
+	ps_bmp-> pixels [2] = (unsigned char *) (s_yuv_pic.pv_data [1]);
+	ps_bmp-> pixels [1] = (unsigned char *) (s_yuv_pic.pv_data [2]);
+
+	ps_bmp-> pitches [0] = s_yuv_pic.v_linesize [0];
+	ps_bmp-> pitches [2] = s_yuv_pic.v_linesize [1];
+	ps_bmp-> pitches [1] = s_yuv_pic.v_linesize [2];
+
+	s_rect.x = 0;
+	s_rect.y = 0;
+	s_rect.w = SCREEN_WIDTH +8;
+	s_rect.h = SCREEN_HEIGHT +8;
+
+	int i = 0, ret = 0, n = 0, n_read = 0;
+
+	while (1) {
+
+		int zoiX = shared->zoiX;
+		int zoiY = shared->zoiY;
+
+		n_read = 0;
+		while (n_read < imgSize) {
+			while (( n = read(0, &buffer[n_read], imgSize - n_read)) > 0) {
+				n_read += n;
+			}
+		}
+
+		i++;
+
+		bloc_t *bloc = malloc(sizeof(bloc_t));
+		bloc_t *blocs = malloc(WIDTH*HEIGHT*sizeof(bloc_t));
+		bloc_t *blocsUp = NULL;
+
+		// ZOI	
+		readBlocs(buffer, WIDTH/2, blocs, 0, HEIGHT-ZOIH, ZOIW, ZOIH);
+		writeBlocs(buffer2, WIDTH, blocs, zoiX, zoiY, ZOIW, ZOIH);
+
+		// LEFT SIDE
+		readBlocs(buffer, WIDTH/2, blocs, 0, 0, zoiX/2, ZOIH);
+		blocsUp = upSampleBlocs(blocs, zoiX/2, ZOIH, 2);		
+		writeBlocs(buffer2, WIDTH, blocsUp, 0, 0, zoiX, HEIGHT);
+		free(blocsUp);
+
+		// RIGHT SIDE
+		readBlocs(buffer, WIDTH/2, blocs, zoiX/2, 0, (ZOIW-zoiX)/2, ZOIH);
+		blocsUp = upSampleBlocs(blocs, (ZOIW-zoiX)/2, ZOIH, 2);
+		writeBlocs(buffer2, WIDTH, blocsUp, zoiX+ZOIW, 0, ZOIW-zoiX, HEIGHT);
+		free(blocsUp);
+
+		// TOP BLOCK
+		readBlocs(buffer, WIDTH/2, blocs, ZOIW/2, 0, ZOIW/2, zoiY/2);
+		blocsUp = upSampleBlocs(blocs, ZOIW/2, zoiY/2, 2);
+		writeBlocs(buffer2, WIDTH, blocsUp, zoiX, 0, ZOIW, zoiY);
+		free(blocsUp);
+
+		// BOTTOM BLOCK
+		readBlocs(buffer, WIDTH/2, blocs, ZOIW/2, zoiY/2, ZOIW/2, (ZOIH-zoiY)/2);
+		blocsUp = upSampleBlocs(blocs, ZOIW/2, (ZOIH-zoiY)/2, 2);
+		writeBlocs(buffer2, WIDTH, blocsUp, zoiX, zoiY+ZOIH, ZOIW, ZOIH-zoiY);
+		free(blocsUp);
 
 
-	SDL_LockYUVOverlay (ps_bmp);
+		SDL_LockYUVOverlay (ps_bmp);
 
-	SDL_DisplayYUVOverlay (ps_bmp, & s_rect);
+		SDL_DisplayYUVOverlay (ps_bmp, & s_rect);
 
-	SDL_UnlockYUVOverlay (ps_bmp);
+		SDL_UnlockYUVOverlay (ps_bmp);
 
-	SDL_Delay (40);		
+		SDL_Delay (40);		
 
-	free(blocs);
-	free(bloc);
-}
+		free(blocs);
+		free(bloc);
+	}
 
-/*
-	 int i = 0, ret = 0;
-	 while (1) {
-
-	 int zoiX = shared->zoiX;
-	 int zoiY = shared->zoiY;
-
-	 n_read = 0;
-	 while (n_read < imgSize) {
-	 while (( n = read(0, &buffer[n_read], imgSize - n_read)) > 0) {
-	 n_read += n;
-	 }
-	 }
-
-	 bloc_t *bloc = malloc(sizeof(bloc_t));
-	 bloc_t *blocs = malloc(WIDTH/2*HEIGHT/2*sizeof(bloc_t));
-	 bloc_t *blocsUp;
-
-// ZOI	
-readBlocs(buffer, blocs, 0, HEIGHT-ZOIH, ZOIW, ZOIH);
-writeBlocs(buffer2, WIDTH, blocs, 0, 0, ZOIW, ZOIH);
-writeBlocs(buffer2, WIDTH, blocs, 0, ZOIH, ZOIW, ZOIH);
-writeBlocs(buffer2, WIDTH, blocs, ZOIW, 0, ZOIW, ZOIH);
-writeBlocs(buffer2, WIDTH, blocs, ZOIW, ZOIH, ZOIW, ZOIH);
-
-i++;
-printf("IMG %d\n", ++shared->imgCount);
-write(pipeToF[1], buffer2, imgSize);
-
-free(blocs);
-free(bloc);
-}
-*/
 }
 
 void *task_network_receiver(void *data) {	
@@ -336,35 +204,26 @@ void *task_network_receiver(void *data) {
 		int len = read(shared->socket, buf, 100);
 		if (len == 0) {
 			//error("ERROR reading socket TCP");
+			printf("Control socket lost! Aborting mission...\n");
+			SDL_Quit();
+			exit(1);
 		}
 		buf[len] = 0;
-		printf("RECV=%s\n",buf);
+		printf("TCPRECV=%s",buf);
 		if (strncmp(buf,"POS;",4) == 0) {
-			printf("Position\n");
 			int frame;
 			int newX;
 			int newY;
 			sscanf(buf,"POS;%d;%d;%d\n", &frame, &newX, &newY);
-			printf("F=%d -> %d,%d\n",frame, newX, newY);
+			printf("Future ZOI position : frame=%d -> %d,%d\n",frame, newX, newY);
 			zoiInfo* nZoi = (zoiInfo*)calloc(1,sizeof(zoiInfo));
 			nZoi->frame=frame;
 			nZoi->x=newX;
 			nZoi->y=newY;
-			printf("Adding...\n");
+			//printf("Adding...\n");
 			addFutureZoi(shared, nZoi);
-			printf("Added\n");
+			//printf("Added\n");
 		}
-		/*
-			 int a;
-			 int b;
-			 sscanf(buf,"%d,%d\n",&a, &b);
-			 printf("a=%d\n",a);
-			 printf("b=%d\n",b);
-			 shared->zoiX=a;
-			 shared->zoiY=b;
-			 sprintf(buf, "POS;%d;%d;%d\n", shared->imgCount, shared->zoiX, shared->zoiY);
-			 write(shared->socket, buf, strlen(buf)); 
-			 */
 	}
 
 }
@@ -389,7 +248,8 @@ void *task_network_sender(void *data) {
 		{
 			case SDL_QUIT:
 				continuer = 0;
-				printf("quit\n");
+				printf("Exit\n");
+				SDL_Quit();
 				exit(0);
 				break;
 			case SDL_KEYDOWN:
